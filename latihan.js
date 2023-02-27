3247863901f1158dea9cb864727f84d7f8cca690
terenkripsi <rizkipratamahermawan@gmail.com>
2023-02-27 21:24:08 +0700

    update js

--- latihan.js
+++ latihan.js
@@ -1,6 +1,8 @@
 var q=55
-var nama = prompt('masukkan nama:')
+var nama = prompt('masukkan nama:');
+for(var i = 0; i < 5; i++) {
 alert('Halo ' + nama + '-kun');
+}
 confirm('okein')
 console.log('Halo!');
 console.log('isi variable q adalah ' + q);
